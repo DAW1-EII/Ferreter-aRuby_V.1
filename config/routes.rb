15b422f7b7448266c848ff8668959e804b5c6948
juanmapl15 <juanma.pl15@gmail.com>
2018-12-17 12:05:36 +0000

    Ferretería V1.3
    Mejora del buscador, rutas paginas, estilo de la página
    
    Co-Authored-By: Óscar Santana Ortega <osaor@users.noreply.github.com>
    Co-Authored-By: davidleon89 <davidleon89@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ Rails.application.routes.draw do
   root :to => "home#index"
   get 'home/index'
 
-  get 'homedosp/index'
-  get 'carrito/compra'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
